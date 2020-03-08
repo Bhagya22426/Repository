256d489c08e4c5c32820d7b3204b3474eb70bb16
Bhagya <bhagya.g2015@gmail.com>
2020-03-08 20:37:38 +0530

    collection

---
+++ bean/UserLoginCridential.java
@@ -0,0 +1,45 @@
+package com.capgemini.librarymanagementsystem.bean;
+
+public class UserLoginCridential {
+	
+	private int userId;
+	private String username;
+	private String name;
+	private String password;
+	private String status;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
